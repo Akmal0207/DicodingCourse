cba931e3df920cb5c26e5bb4acba4e007636f09e
Akmal0207 <145182781+Akmal0207@users.noreply.github.com>
2023-09-19 22:57:38 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,22 +4,38 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
+/* Komentar : Membuat variabel untuk setiap elemen view. */
 
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* Komentar : Membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Komentar : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+   
+   /* Komentar : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /* Komentar : Memastikan bahwa nilai emai dan password sesuai dengan nilai yang tersimpan. */
+    if (email == expectedEmail && password == expectedPassword){
+    
+    
+    /* Komentar : Jika sesuai maka program akan berpindah ke halaman home. */
+      goToHome();
+    }else{
+
+    /* Komentar : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+      showPopUp();
+    }
 });
